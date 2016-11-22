c571c9345c072e6fe506ce219d09260b51348d59
lyn716 <965606089@qq.com>
2016-11-22 23:35:53 +0800

    add task flag sql generator

--- static_spider/init_range_sql.py
+++ static_spider/init_range_sql.py
@@ -11,30 +11,54 @@
 """
 import pymysql
 
-conn = pymysql.connect(
-    db = 'spiderpython',
-    user = 'root',
-    password = 'xingguang@123',
-    host = '123.57.213.217',
-    port = 3306
-)
+def get_conn():
+    conn = pymysql.connect(
+        db = 'spiderpython',
+        user = 'root',
+        password = 'xingguang@123',
+        host = '123.57.213.217',
+        port = 3306
+    )
+    conn.autocommit=True
+    return conn
 
-cur = conn.cursor()
-
-def mark_flag(note_id):
+def mark_flag(left):
+    conn = get_conn()
+    cur = conn.cursor()
+    right = left + 100000
     sql = (
-        'insert into task_flag(note_id)'
-        'VALUES ({})'
-    ).format(note_id)
+        'insert into task_flag(`left`,`right`)'
+        ' VALUES ({},{})'
+    ).format(left,right)
     print(sql)
+    try:
+        cur.execute(sql)
+        conn.commit()
+        print('save ok')
+    except Exception as e:
+        print(str(e))
+    cur.close()
+    conn.close()
+
+
 
 start = 5400000000
 
 end = 5500000000
+
+gap = 100000
+
+
+'''
+lefts = list(range(start,end,gap))
+
+for j in lefts:
+    print(j,j+gap)
+'''
 
 from multiprocessing.dummy import Pool as ThreadPool
 
 
 pool = ThreadPool(16)
 
-pool.map(mark_flag,range(start,end))
+pool.map(mark_flag,range(start,end,gap))
