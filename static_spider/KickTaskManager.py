69d0ea7230e17b10af90423779fff7d349483bec
lyn716 <tonylu716@gmail.com>
2016-11-24 20:43:37 +0800

    del main

--- static_spider/KickTaskManager.py
+++ static_spider/KickTaskManager.py
@@ -54,6 +54,22 @@ def get_unfinished_range():
     conn.close()
     return data
 
+def get_spefic_range(leftest,rightest):
+    conn = get_conn()
+    cur = conn.cursor()
+    sql = 'select `left`,`right`,`id` \
+        from task_flag where is_crawled=0 \
+        and id>{} and id<{} limit 100'.format(
+        get_max_grade(),
+        leftest,rightest
+    )
+    print(sql)
+    cur.execute(sql)
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
+
 def mark_ok(id,success_cot):
     conn = get_conn()
     cur = conn.cursor()
@@ -66,55 +82,38 @@ def mark_ok(id,success_cot):
     cur.close()
     conn.close()
 
-
-def run():
+def run(big_loop=True,leftest=None,rightest=None):
     while(1):
-        range = random.choice(get_unfinished_range())
-        print(range)
-        left = range[0]
-        right = range[1]
-        id = range[2]
-        #mark_ok(id,2)
         try:
-            success_cot = DarenStaticDataGenerator(
-                    left,right).run(
-                mysql=True,
-                thread_cot=64,
-                use_proc_pool=False,
-                use_email=True,
-                dynamic_range_length=right-left,
-                err_print=True,
-                visit_shuffle=False,
-                save_db_type=0,
-                debug=True,
-                save_by_django=False
-            )
-            mark_ok(id,success_cot)
-        except Exception as e:
-            print(str(e))
-       
+            if big_loop:
+                range = random.choice(get_unfinished_range())
+            else:
+                range = random.choice(get_spefic_range(leftest,rightest))
+            print(range)
+            left = range[0]
+            right = range[1]
+            id = range[2]
+            #mark_ok(id,2)
+            try:
+                success_cot = DarenStaticDataGenerator(
+                        left,right).run(
+                    mysql=True,
+                    thread_cot=64,
+                    use_proc_pool=False,
+                    use_email=True,
+                    dynamic_range_length=right-left,
+                    err_print=True,
+                    visit_shuffle=False,
+                    save_db_type=0,
+                    debug=True,
+                    save_by_django=False
+                )
+                mark_ok(id,success_cot)
+            except Exception as e:
+                print(str(e))
+            finally:
+                gc.collect()
+        except:
+            pass
 
-def per_proc_run(proc_id):
-    try:
-        run()
-        gc.collect()
-    except Exception as e:
-        print(str(e))
 
-if __name__=="__main__":
-
-    from multiprocessing import Pool as ProcPool
-
-    use_proc = False
-
-    if use_proc:
-        proc_pool = ProcPool(2)
-
-    import time
-    while(1):
-        if use_proc:
-            proc_pool.map(per_proc_run,range(4))
-            proc_pool.close()
-            proc_pool.join()
-        per_proc_run(1)
-        time.sleep(1)
