79dd8b19dd38f77c5bffff2c3402ec31018af467
lyn716 <tonylu716@gmail.com>
2016-11-29 22:21:04 +0800

    deadlock

--- static_spider/mission3plus.py
+++ static_spider/mission3plus.py
@@ -22,108 +22,169 @@ sys.path.append(
 from api.func import Timer
 from api.DarenPageParser import ProdsInfoGenerator
 from multiprocessing.dummy import Pool as ThreadPool
-import requests,random
+import requests,random,pymysql
 
-
-EX_THREAD_COT = 64
+global index
+index = 0
+EX_THREAD_COT = 32
 
 conn_pool = []
 
-for i in range(100):
-    conn_pool.append(get_conn())
+def init_conn(i):
+    print('init conn: {} ok'.format(i))
+    conn_pool.append({
+        'conn':get_conn(),
+        'status':0
+        })
 
+pool = ThreadPool(32)
+pool.map(init_conn,range(100))
+pool.close()
+pool.join()
+
+
+print('init db pool ok')
 
 def get_random_conn():
-    return random.choice(conn_pool)
+    loop_cot=0
+    while(1):
+        loop_cot+=1
+        busy_cot = 0
+        free_cot = 0
+        for conn in conn_pool:
+            if conn['status']==1:
+                busy_cot += 1
+            else:
+                free_cot += 1
+        '''
+        print(free_cot,busy_cot)
+        '''
+        for conn in conn_pool:
+            if conn['status']==0:
+                conn['status']=1
+                return conn
+        print('pool no free,wait..')
+        time.sleep(2)
 
 def get_unhandled_darenIds():
-    conn = get_random_conn()
+    connObj = get_random_conn()
+    conn = connObj['conn']
     cur = conn.cursor()
-    cur.execute(
-        'select darenId from t_daren_note_task '
-    )
-    ids = [ item[0] for item in cur.fetchall()]
+    try:
+        cur.execute(
+            'select darenId from t_daren_note_task\
+                order by updateTime'
+        )
+        ids = [ item[0] for item in cur.fetchall()]
+    except Exception as e:
+        print(str(e))
+        ids = []
     cur.close()
+    connObj['status']=0
     return ids
 
 def insert_per_good(kvargs):
     return insert_one_good(**kvargs)
 
 def insert_one_good(darenId,darenNoteId,table):
-    conn = get_random_conn()
+    connObj = get_random_conn()
+    conn = connObj['conn']
     cur = conn.cursor()
     sql = "insert into {}(`darenId`,`darenNoteId`) \
         VALUES ('{}','{}')".format(table,darenId,darenNoteId)
     #print(sql)
+    dead_lock = 0
     try:
         cur.execute(sql)
         conn.commit()
         print('insert {} ok'.format(darenNoteId))
         res = True
+    except pymysql.err.IntegrityError:
+        res = False
     except Exception as e:
-        print(str(e))
+        dead_lock = 1
+        print('insert error:{}'.format(str(e)))
         print(sql)
         res = False
     cur.close()
+    if dead_lock:
+        conn.close()
+        conn_pool.remove(connObj)
+        conn_pool.append({
+            'conn': get_conn(),
+            'status': 1
+        })
+    else:
+        connObj['status'] = 0
     return res
 
 def get_good_ids(darenHomeUrl):
-    for i in range(10):
+    for i in range(1):
         try:
-            req = requests.get(darenHomeUrl,timeout=10)
+            req = requests.get(darenHomeUrl,timeout=5)
             html = req.text
             break
         except:
             print('req error')
     try:
         ids = ProdsInfoGenerator(html_source=html).to_id_list()
-        print(ids)
         return ids
     except IndexError:
         return []
+    except:
+        return []
 
-def mark_task_ok(darenId,timeuse,crawl_cot,insert_cot):
-    conn = get_random_conn()
+def mark_task_ok(darenId,db_use,crawl_cot,insert_cot,status,req_use):
+    connObj = get_random_conn()
+    conn = connObj['conn']
     cur = conn.cursor()
     sql = (
         "update t_daren_note_task set `timeuse`={},"
         "`crawl_cot`={},`insert_cot`={} "
         "where darenId='{}'"
-    ).format(timeuse,crawl_cot,insert_cot,darenId)
+    ).format(db_use+req_use,crawl_cot,insert_cot,darenId)
+    status += '\t{}\t{}'.format(req_use,db_use)
     try:
         cur.execute(sql)
         conn.commit()
-        print('update {} ok '.format(darenId))
+        print('{}\tupdate ok '.format(status))
     except Exception as e:
-        print(str(e))
+        print('update error:{}'.format(str(e)))
         print(sql)
     cur.close()
+    connObj['status']=0
 
 def crawl_per_daren(darenId):
+    global index
+    index += 1
     tm = Timer()
     tm.start()
     darenHomeUrl = 'http://uz.taobao.com/home/{}'.format(darenId)
-    print(darenHomeUrl)
     darenNoteIds = get_good_ids(darenHomeUrl)
-    print(len(darenNoteIds))
-    pool = ThreadPool(32)
+    status = '{}\t{}\t{}\t{}'\
+        .format(darenId,darenHomeUrl,index,len(darenNoteIds))
+    pool = ThreadPool(8)
     goods = []
     for darenNoteId in darenNoteIds:
-        print(darenNoteId)
         goods.append({
             'darenId':darenId,
             'darenNoteId':darenNoteId,
             'table':'t_daren_goodinfo_02',
         })
+    tm.end()
+    req_use = tm.gap
+    tm.start()
     res = pool.map(insert_per_good,goods)
     tm.end()
     pool.close()
     pool.join()
     kvargs = {
         'darenId': darenId,
-        'timeuse': round(tm.gap/EX_THREAD_COT,2),
+        'db_use': round(tm.gap,2),
         'crawl_cot': len(darenNoteIds),
-        'insert_cot':res.count(True)
+        'insert_cot':res.count(True),
+        'status': status,
+        'req_use': req_use
     }
     mark_task_ok(**kvargs)
 
@@ -131,9 +192,11 @@ def crawl_per_daren(darenId):
 if __name__=="__main__":
     import time
     while(1):
+        index = 0
         darenIds = get_unhandled_darenIds()
         ex_pool = ThreadPool(EX_THREAD_COT)
         ex_pool.map(crawl_per_daren,darenIds)
+        print('one time loop end..')
         ex_pool.close()
         ex_pool.join()
         time.sleep(1)
